e332b032b10656db1fb6f517086af2844ce61654
Dylan Hepworth <dylanhepworth@gmail.com>
2018-06-14 22:22:52 -0700

    Add morgan for logging

--- server/app.js
+++ server/app.js
@@ -4,11 +4,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const jwt = require('helpers/jwt');
 const errorHandler = require('helpers/error-handler');
 
 const users = require('./routes/users');
 
+app.use(morgan('tiny'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({
